a4b8a7772649e9a861ad6ca7fc10387481bfc757
Kyle Cesare <kcesare@gmail.com>
2013-10-17 13:21:44 -0700

    Add robots.

--- iarc_sim.py
+++ iarc_sim.py
@@ -1,4 +1,16 @@
+import numpy as np
 import pygame
+
+class Robot:
+    def __init__(self, (x, y)):
+        self.pos = np.array([float(x), float(y)])
+
+        self.velocity = np.random.rand(2) - 0.5
+        # TODO: robots always travel at 0.33 m/s
+        # self.velocity = np.linalg.norm(self.velocity)
+
+    def update(self, tick_length):
+        self.pos += self.velocity * tick_length
 
 def meters_to_pixels(meters):
     return int(round(meters * 20))
@@ -26,18 +38,26 @@ def draw_robot(screen, color, (x, y)):
 pygame.init()
 screen = pygame.display.set_mode((meters_to_pixels(20) + 5, meters_to_pixels(20) + 5))
 
+robots = []
+for x in range(9, 12):
+    for y in range(9, 12):
+        if x == 10 and y == 10:
+            continue
+
+        robots.append(Robot((x, y)))
+
 clock = pygame.time.Clock()
 while True:
     clock.tick(60)
+    # TODO: blit background
+    screen.fill((0, 0, 0))
 
     draw_arena(screen)
-    draw_robot(screen, (0, 0, 255), (meters_to_pixels(11), meters_to_pixels(9)))
-    draw_robot(screen, (0, 0, 255), (meters_to_pixels(9),  meters_to_pixels(11)))
-    draw_robot(screen, (0, 0, 255), (meters_to_pixels(11), meters_to_pixels(11)))
-    draw_robot(screen, (0, 0, 255), (meters_to_pixels(9),  meters_to_pixels(9)))
-    draw_robot(screen, (0, 0, 255), (meters_to_pixels(10), meters_to_pixels(9)))
-    draw_robot(screen, (0, 0, 255), (meters_to_pixels(9),  meters_to_pixels(10)))
-    draw_robot(screen, (0, 0, 255), (meters_to_pixels(10), meters_to_pixels(11)))
-    draw_robot(screen, (0, 0, 255), (meters_to_pixels(11), meters_to_pixels(10)))
+
+    for robot in robots:
+        robot.update(1.0 / 60.0)
+
+        px, py = meters_to_pixels(robot.pos[0]), meters_to_pixels(robot.pos[1])
+        draw_robot(screen, (0, 0, 255), (px, py))
 
     pygame.display.flip()
